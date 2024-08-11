9924a754192d068b54383a8f002c4d35485762af
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-11 18:01:10 +0900

    Fix typo

--- talos/patch_machine_config.sh
+++ talos/patch_machine_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 talosctl machineconfig patch worker.yaml \
-    --patch @talos_patches/disable-cni.yaml \
-    --patch @talos_patches/data-path-mount.yaml \
+    --patch @patches/disable-cni.yaml \
+    --patch @patches/data-path-mount.yaml \
     -o worker.yaml
 
 talosctl machineconfig patch controlplane.yaml \
-    --patch @talos_patches/disable-cni.yaml \
-    --patch @talos_patches/data-path-mount.yaml \
+    --patch @patches/disable-cni.yaml \
+    --patch @patches/data-path-mount.yaml \
     -o controlplane.yaml
