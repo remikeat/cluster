bf2d60b6c65795652522b33e52b1ebc2e368a548
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-17 00:36:27 +0900

    Fix parameters

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_url')
-REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_revision')
-REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_path')
-VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .values')
-ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .args')
-SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .secrets')
+REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_url").value')
+REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").value')
+REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").value')
+VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values").value')
+ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").value')
+SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").value')
 git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
 helm template $ARGS -f $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
