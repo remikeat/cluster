d205fd078ad9460549d0eefa2c9d82aae4a61a97
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-16 23:56:28 +0900

    Fix parameters should be array

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.repo_url')
-REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.repo_revision')
-REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.repo_path')
-VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.values')
-ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.args')
-SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.secrets')
+REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_url')
+REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_revision')
+REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .repo_path')
+VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .values')
+ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .args')
+SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r ' add | .secrets')
 git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
 helm template $ARGS -f $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
