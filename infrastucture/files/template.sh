a614deb49660faae1c3071484a7604634274215a
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-17 23:23:18 +0900

    Rename plugin to custom-cmp

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_url").string')
-REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").string')
-REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").string')
-VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values").string')
-ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").string')
-SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").string')
-git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
-helm template $ARGS -f $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
+
+if [ -f "Chart.yaml" ]; then
+    REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_url").string')
+    REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").string')
+    REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").string')
+    VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values").string')
+    ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").string')
+    SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").string')
+    git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
+    helm template $ARGS -f $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
+elif [ -f "kustomization.yaml" ]; then
+    kustomize .
+else
+    >&2 echo "Neither Chart.yaml nor kustomization.yaml found."
+fi
