94942e30e31668e63bb1690d559c0776e088a557
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-18 12:58:42 +0900

    Improve plugin script

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -5,10 +5,16 @@ if [ -f "Chart.yaml" ]; then
     REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").string')
     REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").string')
     VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values").string')
+    if [ -n "$REPO_URL" ]; then
+        git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
+    fi
+
     ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").string')
     SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").string')
-    git archive --remote=$REPO_URL $REPO_REVISION:$REPO_PATH $VALUES | tar -x
-    helm template $ARGS -f $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
+    if [ -n "$SECRETS" ]; then
+        SECRETS="-f $SECRETS"
+    fi
+    helm template $ARGS $SECRETS -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
 elif [ -f "kustomization.yaml" ]; then
     kustomize build
 else
