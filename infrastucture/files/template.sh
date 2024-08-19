8a508674e4ba6d4b28739d98bf1e2c710fffef75
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-19 22:53:53 +0900

    Simplify parameters

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -4,23 +4,19 @@ if [ -f "Chart.yaml" ]; then
     cmd=(helm template)
     REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_url").string')
     REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").string')
-    REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").string')
-    VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values").string')
+    REPO_VALUES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_values").string')
     if [ -n "$VALUES" ]; then
         git clone $REPO_URL custom_values
         cd custom_values
         git checkout $REPO_REVISION
         cd -
-        cp custom_values/$REPO_PATH/$VALUES custom_values.yaml
-        VALUES="custom_values.yaml"
-        cmd+=(-f $VALUES)
+        cmd+=(-f custom_values/$REPO_VALUES)
     fi
 
     ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").string')
     SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").string')
     if [ -n "$SECRETS" ]; then
-        SECRETS="bw-secrets/$SECRETS"
-        cmd+=(-f $SECRETS)
+        cmd+=(-f bw-secrets/$SECRETS)
     fi
     cmd+=(-n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .)
     "${cmd[@]}"
