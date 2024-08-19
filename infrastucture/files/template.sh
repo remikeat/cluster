27998605bea702f83b62e4b309f3ded9b65ee785
Remi KEAT <remikeat@users.noreply.github.com>
2024-08-19 22:37:37 +0900

    Use bash

--- infrastucture/files/template.sh
+++ infrastucture/files/template.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f "Chart.yaml" ]; then
+    cmd=(helm template)
     REPO_URL=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_url").string')
     REPO_REVISION=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_revision").string')
     REPO_PATH=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "repo_path").string')
@@ -11,15 +12,18 @@ if [ -f "Chart.yaml" ]; then
         git checkout $REPO_REVISION
         cd -
         cp custom_values/$REPO_PATH/$VALUES custom_values.yaml
-        VALUES="-f custom_values.yaml"
+        VALUES="custom_values.yaml"
+        cmd+=(-f $VALUES)
     fi
 
     ARGS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "args").string')
     SECRETS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "secrets").string')
     if [ -n "$SECRETS" ]; then
-        SECRETS="-f bw-secrets/$SECRETS"
+        SECRETS="bw-secrets/$SECRETS"
+        cmd+=(-f $SECRETS)
     fi
-    helm template $ARGS $SECRETS $VALUES -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .
+    cmd+=(-n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME .)
+    "${cmd[@]}"
 elif [ -f "kustomization.yaml" ]; then
     kustomize build
 else
