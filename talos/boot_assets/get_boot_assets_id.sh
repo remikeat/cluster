dd860bc321a5ac4fff9522709102778b010fd01a
Remi KEAT <remikeat@users.noreply.github.com>
2024-09-01 10:38:22 +0900

    Output image link

--- talos/boot_assets/get_boot_assets_id.sh
+++ talos/boot_assets/get_boot_assets_id.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -s -X POST --data-binary @assets.yaml https://factory.talos.dev/schematics | jq -r .id
+
+id=$(curl -s -X POST --data-binary @assets.yaml https://factory.talos.dev/schematics | jq -r .id)
+link=$(echo "factory.talos.dev/installer/$id:v1.7.6")
+echo "$link" | xclip -selection clipboard
+echo "$link"
