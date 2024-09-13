bb7e167e7da3e66acac0d9ef020d21f457cc02ed
Remi KEAT <remikeat@users.noreply.github.com>
2024-09-13 18:07:26 +0900

    Update talos config

--- talos/patch_machine_config.sh
+++ talos/patch_machine_config.sh
@@ -2,9 +2,13 @@
 talosctl machineconfig patch worker.yaml \
     --patch @patches/disable-cni.yaml \
     --patch @patches/data-path-mount.yaml \
+    --patch @patches/install.yaml \
+    --patch @patches/metrics-server.yaml \
     -o worker.yaml
 
 talosctl machineconfig patch controlplane.yaml \
     --patch @patches/disable-cni.yaml \
     --patch @patches/data-path-mount.yaml \
+    --patch @patches/install.yaml \
+    --patch @patches/metrics-server.yaml \
     -o controlplane.yaml
