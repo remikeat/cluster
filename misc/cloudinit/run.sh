bdf94302498909ff7b1968821e3e6b32210845a0
Remi KEAT <remikeat@users.noreply.github.com>
2024-09-27 01:08:36 +0900

    Fix image

--- misc/cloudinit/run.sh
+++ misc/cloudinit/run.sh
@@ -1,6 +1,5 @@
 #/bin/bash
-#IMG=noble-server-cloudimg-amd64.img
-IMG=nocloud_alpine-3.20.3-x86_64-bios-tiny-r0.qcow2
+IMG=noble-server-cloudimg-amd64.img
 cloud-localds seed.img user-data meta-data
 qemu-system-x86_64 -m 1024 -net nic -net user,hostfwd=tcp::10022-:22 -nographic \
     -drive file=$IMG,index=0,format=qcow2,media=disk \
