e54a7c5f01fa8e6d517733ad7b5e54c1d32da19e
Remi KEAT <remikeat@users.noreply.github.com>
2024-10-05 23:41:10 +0900

    Debug KRM issue

--- argocd/apps/supabase/git-generator.sh
+++ argocd/apps/supabase/git-generator.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-resourceList=$(cat)
+# resourceList=$(cat)
 
-repoURL=$(echo "$resourceList" | yq e '.functionConfig.spec.repoURL' - )
-path=$(echo "$resourceList" | yq e '.functionConfig.spec.path' - )
-targetRevision=$(echo "$resourceList" | yq e '.functionConfig.spec.targetRevision' - )
-valuesFile=$(echo "$resourceList" | yq e '.functionConfig.spec.valuesFile' - )
-name=$(echo "$resourceList" | yq e '.functionConfig.spec.name' - )
-namespace=$(echo "$resourceList" | yq e '.functionConfig.spec.namespace' - )
+# repoURL=$(echo "$resourceList" | yq e '.functionConfig.spec.repoURL' - )
+# path=$(echo "$resourceList" | yq e '.functionConfig.spec.path' - )
+# targetRevision=$(echo "$resourceList" | yq e '.functionConfig.spec.targetRevision' - )
+# valuesFile=$(echo "$resourceList" | yq e '.functionConfig.spec.valuesFile' - )
+# name=$(echo "$resourceList" | yq e '.functionConfig.spec.name' - )
+# namespace=$(echo "$resourceList" | yq e '.functionConfig.spec.namespace' - )
 
-git clone -q --single-branch --branch=$targetRevision $repoURL repo
-helm template $name -n $namespace -f $valuesFile repo/$path 2>/dev/null
-rm -rf repo
+# git clone -q --single-branch --branch=$targetRevision $repoURL repo
+# helm template $name -n $namespace -f $valuesFile repo/$path 2>/dev/null
+# rm -rf repo
