613efadf66dc58780d807811235e32132cbc97c1
Remi KEAT <remikeat@users.noreply.github.com>
2024-10-07 22:30:13 +0900

    Replace secrets in values.yaml

--- argocd/apps/supabase/git-generator.sh
+++ argocd/apps/supabase/git-generator.sh
@@ -9,6 +9,7 @@ valuesFile=$(echo "$resourceList" | yq e '.functionConfig.spec.valuesFile' - )
 name=$(echo "$resourceList" | yq e '.functionConfig.spec.name' - )
 namespace=$(echo "$resourceList" | yq e '.functionConfig.spec.namespace' - )
 
+argocd-vault-plugin generate $valuesFile > values.yaml
 git clone -q --single-branch --branch=$targetRevision $repoURL repo
 helm template $name -n $namespace -f $valuesFile repo/$path 2>/dev/null
 rm -rf repo
