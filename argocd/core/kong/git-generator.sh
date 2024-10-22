1aef501780af951e27513d871a3bdef31549578f
Remi KEAT <remikeat@users.noreply.github.com>
2024-10-22 21:15:02 +0900

    Add patch

--- argocd/core/kong/git-generator.sh
+++ argocd/core/kong/git-generator.sh
@@ -5,12 +5,14 @@ resourceList=$(cat)
 repoURL=$(echo "$resourceList" | yq e '.functionConfig.spec.repoURL' - )
 path=$(echo "$resourceList" | yq e '.functionConfig.spec.path' - )
 targetRevision=$(echo "$resourceList" | yq e '.functionConfig.spec.targetRevision' - )
+patch=$(echo "$resourceList" | yq e '.functionConfig.spec.patch' - )
 valuesFile=$(echo "$resourceList" | yq e '.functionConfig.spec.valuesFile' - )
 name=$(echo "$resourceList" | yq e '.functionConfig.spec.name' - )
 namespace=$(echo "$resourceList" | yq e '.functionConfig.spec.namespace' - )
 skipCrds=$(echo "$resourceList" | yq e '.functionConfig.spec.skipCrds' - )
 
 git clone -q --single-branch --branch=$targetRevision $repoURL $name &> /dev/null
+patch -d $name -p1 < $patch &> /dev/null
 helmCommand=("helm" "template" "$name" "-n" "$namespace" "-f" "$valuesFile" "$name/$path")
 if [ "$skipCrds" = "true" ]; then
     helmCommand+=("--skip-crds")
