e2c7afa83889de365e06c2a13d34d06dc823c668
Remi KEAT <remikeat@users.noreply.github.com>
2024-12-09 22:22:39 +0900

    Add telepresence

---
+++ argocd/dev/telepresence/git-generator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+resourceList=$(cat)
+
+repoURL=$(echo "$resourceList" | yq e '.functionConfig.spec.repoURL // ""' - )
+path=$(echo "$resourceList" | yq e '.functionConfig.spec.path // ""' - )
+targetRevision=$(echo "$resourceList" | yq e '.functionConfig.spec.targetRevision // ""' - )
+patch=$(echo "$resourceList" | yq e '.functionConfig.spec.patch // ""' - )
+valuesFile=$(echo "$resourceList" | yq e '.functionConfig.spec.valuesFile // ""' - )
+name=$(echo "$resourceList" | yq e '.functionConfig.spec.name // ""' - )
+namespace=$(echo "$resourceList" | yq e '.functionConfig.spec.namespace // ""' - )
+skipCrds=$(echo "$resourceList" | yq e '.functionConfig.spec.skipCrds // ""' - )
+apiVersions=$(echo "$resourceList" | yq e '.functionConfig.spec.apiVersions // []' - )
+
+git clone -q --single-branch --branch=$targetRevision $repoURL $name &> /dev/null
+if [ ! -z "$patch" ]; then
+    patch -d $name -p1 < $patch &> /dev/null
+fi
+helmCommand=("helm" "template" "$name" "-n" "$namespace")
+if [ ! -z "$valuesFile" ]; then
+    helmCommand+=("-f" "$valuesFile")
+fi
+if [ "$skipCrds" = "true" ]; then
+    helmCommand+=("--skip-crds")
+fi
+for apiVersion in $(echo "$apiVersions" | yq e '.[]' -); do
+    helmCommand+=("-a" "$apiVersion")
+done
+helmCommand+=("$name/$path")
+"${helmCommand[@]}" 2>/dev/null
+rm -rf $name
